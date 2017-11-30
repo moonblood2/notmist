f85e1549fd7cf4be4e731e706989f65b1afd4a3b
moonblood2 <moonblood2@gmail.com>
2017-11-30 10:56:21 +0700

    Update notmist_why.py

--- notmist_why.py
+++ notmist_why.py
@@ -8,62 +8,67 @@ os.chdir('C:\\Users\\ASUS\\Desktop\\deeplearn')
 with open('notMNIST.pickle','rb') as f:
     save = pickle.load(f)
     train_dataset = save['train_dataset']
-    train_labels = save['train_labels'] 
-    valid_dataset = save['valid_dataset']
-    valid_labels = save['valid_labels']
-    test_dataset = save['test_dataset']
-    test_labels = save['test_labels']
-    del save
+    train_labels =save['train_labels']
+    valid_dataset =save['valid_dataset']
+    valid_labels =save['valid_labels']
+    test_dataset =save['test_dataset']
+    test_labels =save['test_labels']
 
-print('train:',train_dataset.shape,train_labels.shape)
-print('valid:',valid_dataset.shape,valid_labels.shape)
-print('test:',test_dataset.shape,test_labels.shape)
-
-image_size = 28
+img_size = 28
 num_labels = 10
 
-def reformat(dataset,labels):
-    dataset = dataset.reshape((-1,image_size**2)).astype(np.float32)
+def reformat(data,labels):
+    data = data.reshape(-1,img_size**2).astype(np.float32)
     labels = (np.arange(num_labels)==labels[:,None]).astype(np.float32)
-    return dataset, labels
+    return data,labels
 
 train_dataset,train_labels = reformat(train_dataset,train_labels)
 valid_dataset,valid_labels = reformat(valid_dataset,valid_labels)
 test_dataset,test_labels = reformat(test_dataset,test_labels)
 
+batch_size = 128
+
 graph = tf.Graph()
+
 with graph.as_default():
 
-    tf_train_dataset = tf.constant(train_dataset[:10000,:])
-    tf_train_labels = tf.constant(train_labels[:10000])
+    tf_train_dataset = tf.placeholder(tf.float32, shape = (batch_size,img_size**2))
+    tf_train_labels = tf.placeholder(tf.float32, shape = (batch_size,num_labels))
     tf_valid_dataset = tf.constant(valid_dataset)
     tf_test_dataset = tf.constant(test_dataset)
 
-    weights = tf.Variable(tf.truncated_normal([image_size**2,num_labels]))
+    weights = tf.Variable(tf.truncated_normal([img_size**2,num_labels]))
     biases = tf.Variable(tf.zeros([num_labels]))
 
     logits = tf.matmul(tf_train_dataset,weights)+biases
-    loss = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(logits=logits,labels=tf_train_labels))
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = tf_train_labels,logits=logits))
 
     optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
-    
-    train_prediction = tf.nn.softmax(logits)
-    valid_prediction = tf.nn.softmax(tf.matmul(tf_valid_dataset, weights) + biases)
-    test_prediction = tf.nn.softmax(tf.matmul(tf_test_dataset, weights) + biases)
 
-num_steps = 801
+    train_predictions = tf.nn.softmax(logits)
+    valid_predictions = tf.nn.softmax(tf.matmul(tf_valid_dataset,weights)+biases)
+    test_predictions = tf.nn.softmax(tf.matmul(tf_test_dataset,weights)+biases)
 
-def accuracy(predictions, labels):
-    return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))/ predictions.shape[0])
+num_steps = 3001
 
-with tf.Session(graph = graph) as session:
+def accuracy(predictions,labels):
+    return 100.0*(np.sum(np.argmax(predictions,1)==np.argmax(labels,1))/labels.shape[0])
+
+with tf.Session(graph = graph) as sess:
+
     tf.initialize_all_variables().run()
-    print('Initialized')
+    print('initialized')
+
     for step in range(num_steps):
-        _, l, predictions = session.run([optimizer, loss, train_prediction])
-        if (step % 100 == 0):
-            print('Loss at step %d: %f' % (step, l))
-            print('Training accuracy: %.1f%%' % accuracy(predictions, train_labels[:10000, :]))
-            print('Validation accuracy: %.1f%%' % accuracy(valid_prediction.eval(), valid_labels))
-    print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
+        offset = np.random.randint(0,high = train_labels.shape[0]-batch_size)
+        batch_dataset = train_dataset[offset:(offset+batch_size)]
+        batch_labels = train_labels[offset:(offset+batch_size)]
+        feed_dict = {tf_train_dataset:batch_dataset, tf_train_labels:batch_labels}
+
+        _,l,predictions = sess.run([optimizer,loss,train_predictions],feed_dict=feed_dict)
+
+        if step%500 == 0:
+            print('loss',l)
+            print('train acc', accuracy(predictions,batch_labels))
+            print('valid acc', accuracy(valid_predictions.eval(),valid_labels))
+    print('test acc', accuracy(test_predictions.eval(),test_labels))
